1754171c813c4f8d4376c19c179bc6ac5af70700
michael montoya <michaelmontoya@michaels-MacBook-Pro.local>
2022-05-28 15:18:20 -0700

    linked css and created js variables

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+var table = document.getElementById('planner');
+var cells = table.getElementsByTagName('td');
+
+console.log(cells)
+// $(function () {
+//     $("td").dblclick(function () {
+//         var OriginalContent = $(this).text();
+//         $(this).addClass("cellEditing");
+//         $(this).html("<input type='text' value='" + OriginalContent + "' />");
+//         $(this).children().first().focus();
+//         $(this).children().first().keypress(function (e) {
+//             if (e.which == 13) {
+//                 var newContent = $(this).val();
+//                 $(this).parent().text(newContent);
+//                 $(this).parent().removeClass("cellEditing");
+//             }
+//         });
+//     $(this).children().first().blur(function(){
+//         $(this).parent().text(OriginalContent);
+//         $(this).parent().removeClass("cellEditing");
+//     });
+//         $(this).find('input').dblclick(function(e){
+//             e.stopPropagation(); 
+//         });
+//     });
+// });
