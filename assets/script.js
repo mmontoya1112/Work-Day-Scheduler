90bba104440c9835a17e81ba7940e42a2c0ff86f
michael montoya <michaelmontoya@michaels-MacBook-Pro.local>
2022-05-29 14:56:37 -0700

    created a schedule function using js and jquery, started to make a styling for the hours

--- assets/script.js
+++ assets/script.js
@@ -2,25 +2,69 @@ var currentMoment = moment()
 var currentDay = document.getElementById('currentDay')
 currentDay.innerHTML = currentMoment.format('MMMM Do YYYY, h:mm:ss a')
 
+var workDay = [
+    '9 AM',
+    '10 AM',
+    '11 AM',
+    '12 PM',
+    '1 PM',
+    '2 PM',
+    '3 PM',
+    '4 PM',
+    '5 PM',
+]
 
-var table = document.getElementById('planner');
-var cells = table.getElementsByTagName('td');
+var workDayValue = [
+    09, 10, 11, 12, 13, 14, 15, 16, 17,
+]
 
-for (var i = 0; i < cells.length; i++) {
-    cells[i].addEventListener('click', function(){
-        console.log('clicked');
-        var input = document.createElement('textarea');
-        cells.append(input)
-        // input.setAttribute('type', 'text');
-        // input.value = this.innerHTML;
-        // input.style.backgroundColor = "LightGoldenRodYellow";
+var timeBlock;
+var hourBlock;
+var eventBlock;
+var saveButton;
 
-        // this.innerHTML = '';
-        // this.append(input);
-        // this.firstElementChild.select();
+function createSchedule(){
+    $.each(workDay, function(index, time){
 
-    }
-)} 
+        //creating rows for our time blocks//
+        timeBlock = $('<div>')
+        timeBlock.addClass('row')
 
-// it needs to be in local storage
-// localStorage.setItem('mostRecentScore', score)
+        //creating section for hour block//
+        hourBlock = $('<div>')
+        hourBlock.addClass('hour col-2 col-lg-1')
+        hourBlock.css('text-align', 'center', 'padding-top', '31px')
+        hourBlock.text(time)
+        timeBlock.append(hourBlock)
+
+        //creating event section//
+        eventBlock = $('<textarea>')
+        eventBlock.addClass('col-8 col-lg-10')
+        eventBlock.attr('id', workDayValue[index])
+        timeBlock.append(eventBlock)
+
+        //creating save button sections//
+        saveButton = $('<button>')
+        saveButton.addClass('saveBtn fa fa-save col-2 col-lg-1')
+        saveButton.attr('id', workDayValue[index])
+        saveButton.attr('type', 'submit')
+        saveButton.css('font-size', '24px')
+        timeBlock.append(saveButton)
+
+        $('.container').append(timeBlock)
+    })
+}
+function styleSchedule(){
+    $.each(workDayValue, function (index, time){
+        eventBlock = $("#" + workDayValue[index]);
+
+// styling for past hours
+    if ((parseInt(taskBlock[0].id) < currentMoment.hour())) {
+        taskBlock.addClass("past");
+    }  
+    //add else if for present and future time here
+    })
+}
+
+
+createSchedule()
