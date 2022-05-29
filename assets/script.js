e1d674208742856139de55af22df463df0a2ab07
michael montoya <michaelmontoya@michaels-MacBook-Pro.local>
2022-05-28 20:17:20 -0700

    added date and time and editable boxes

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,26 @@
+var currentMoment = moment()
+var currentDay = document.getElementById('currentDay')
+currentDay.innerHTML = currentMoment.format('MMMM Do YYYY, h:mm:ss a')
+
+
 var table = document.getElementById('planner');
 var cells = table.getElementsByTagName('td');
 
-console.log(cells)
-
 for (var i = 0; i < cells.length; i++) {
-    cells[i].onClick = function(){
-        var input = document.createElement('input');
-        input.setAttribute('type', 'text');
-        input.value = this.innerHTML;
-        input.style.backgroundColor = "LightGoldenRodYellow"
+    cells[i].addEventListener('click', function(){
+        console.log('clicked');
+        var input = document.createElement('textarea');
+        cells.append(input)
+        // input.setAttribute('type', 'text');
+        // input.value = this.innerHTML;
+        // input.style.backgroundColor = "LightGoldenRodYellow";
+
+        // this.innerHTML = '';
+        // this.append(input);
+        // this.firstElementChild.select();
+
     }
-}
-// $(function () {
-//     $("td").dblclick(function () {
-//         var OriginalContent = $(this).text();
-//         $(this).addClass("cellEditing");
-//         $(this).html("<input type='text' value='" + OriginalContent + "' />");
-//         $(this).children().first().focus();
-//         $(this).children().first().keypress(function (e) {
-//             if (e.which == 13) {
-//                 var newContent = $(this).val();
-//                 $(this).parent().text(newContent);
-//                 $(this).parent().removeClass("cellEditing");
-//             }
-//         });
-//     $(this).children().first().blur(function(){
-//         $(this).parent().text(OriginalContent);
-//         $(this).parent().removeClass("cellEditing");
-//     });
-//         $(this).find('input').dblclick(function(e){
-//             e.stopPropagation(); 
-//         });
-//     });
-// });
+)} 
+
+// it needs to be in local storage
+// localStorage.setItem('mostRecentScore', score)
